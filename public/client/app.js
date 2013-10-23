44a4961a83d3e2c0fbffe77951bdf0922f72cba4
Hack Reactor Students <github@hackreactor.com>
2013-10-23 16:23:30 -0700

    Combines two views in a page for the shortener box and the index page. Also deals with path nonsense for passing all the specs.

--- public/client/app.js
+++ public/client/app.js
@@ -2,23 +2,27 @@ window.Shortly = Backbone.View.extend({
 
   template: _.template(' \
       <h1>Shortly</h1> \
-      <div class="navigation"> \
-      <ul> \
-        <li><a href="#" class="index">All Links</a></li> \
-        <li><a href="#" class="create">Shorten</a></li> \
-      </ul> \
-      </div> \
+      <div id="toplink"></div> \
       <div id="container"></div>'
   ),
 
+      //   <div class="navigation"> \
+      // <ul> \
+      //   <li><a href="#" class="index">All Links</a></li> \
+      //   <li><a href="#" class="create">Shorten</a></li> \
+      // </ul> \
+      // </div> \
+
   events: {
-    "click li a.index":  "renderIndexView",
-    "click li a.create": "renderCreateView"
+    "submit #toplink":  "renderIndexView"
+    // "click li a.create": "renderIndexView"//,
+    // "click li a.filter": "renderFilterView"
   },
 
   initialize: function(){
     console.log( "Shortly is running" );
     $('body').append(this.render().el);
+    this.renderCreateView();
     this.renderIndexView(); // default view
   },
 
@@ -32,15 +36,22 @@ window.Shortly = Backbone.View.extend({
     var links = new Shortly.Links();
     var linksView = new Shortly.LinksView( {collection: links} );
     this.$el.find('#container').html( linksView.render().el );
-    this.updateNav('index');
+    // this.updateNav('index');
   },
 
   renderCreateView: function(e){
     e && e.preventDefault();
     var linkCreateView = new Shortly.LinkCreateView();
-    this.$el.find('#container').html( linkCreateView.render().el );
-    this.updateNav('create');
+    this.$el.find('#toplink').html( linkCreateView.render().el );
+    // this.updateNav('create');
   },
+
+  // renderFilterView: function(e){
+  //   e && e.preventDefault();
+  //   var filterCreateView = new Shortly.FilterView();
+  //   this.$el.find('#container').html ( linksView.render().el );
+  //   this.updateNav('filter');
+  // },
 
   updateNav: function(className){
     this.$el.find('.navigation li a')
--- public/client/filterView.js
+++ public/client/filterView.js
@@ -1,12 +1,10 @@
-Shortly.LinkView = Backbone.View.extend({
-
+Shortly.FilterView = Backbone.View.extend({
   className: 'link',
 
   template: _.template(' \
       <img src="/redirect_icon.png"/> \
       <div class="info"> \
         <div class="visits"><span class="count"><%= visits %></span>Visits</div> \
-        <div class="timestamp"><span class="time">Date created: <%= updated_at %></span></div> \
         <div class="title"><%= title %></div> \
         <div class="original"><%= url %></div> \
         <a href="<%= base_url %>/<%= code %>"><%= base_url %>/<%= code %></a> \
@@ -17,5 +15,4 @@ Shortly.LinkView = Backbone.View.extend({
     this.$el.html( this.template(this.model.attributes) );
     return this;
   }
-
 });
